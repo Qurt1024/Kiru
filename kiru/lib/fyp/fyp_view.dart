3862d63a9feee93c115f610b36005b8b7e7076f9
Qurt <leilasal2007@gmail.com>
2025-12-07 22:22:31 +0500

    added fyp page

--- kiru/lib/fyp/fyp_view.dart
+++ kiru/lib/fyp/fyp_view.dart
@@ -1,75 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:kiru/colors.dart';
 import 'package:kiru/images.dart';
 
-class FypView extends StatelessWidget{
+class FypView extends StatelessWidget {
   const FypView({super.key});
 
-static const List<String> images = [
-  AppImages.fypOne,
-  AppImages.fypTwo,
-  AppImages.fypThree,
-  AppImages.fypFour,
-];
- Widget postGrid(String imageName){
-  return Stack(
-  children: [
-    Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(14),
-        image: DecorationImage(image: AssetImage(imageName),
-        fit: BoxFit.cover,
-        )
-        
-      ),
-    ),
-    Positioned(
-      bottom: 8,
-      right: 8,
-      child: Container(
-        padding: EdgeInsets.all(6),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle,
-          ),
-          child: Icon(
-            Icons.favorite_border,
-            size: 20,
-            color: Colors.black,
-          ),
+  static const List<Map<String, dynamic>> images = [
+    {
+      'image': AppImages.fypOne,
+      'height': 280.0,
+      'title': 'Звезда Токио',
+      'subtitle': 'от HummusChan19',
+      'verified': true
+    },
+    {
+      'image': AppImages.fypTwo,
+      'height': 280.0,
+      'title': 'Private Zone',
+      'subtitle': 'от StellaBloom',
+      'verified': true
+    },
+    {
+      'image': AppImages.fypThree,
+      'height': 280.0,
+      'title': 'Уличная мода',
+      'subtitle': 'от Fashionmaster2012',
+      'verified': false
+    },
+    {
+      'image': AppImages.fypFour,
+      'height': 280.0,
+      'title': 'Темный стиль',
+      'subtitle': 'от Xxx_StyleMaster_xxX',
+      'verified': false
+    },
+  ];
+
+  Widget postGrid(String imageName, double height, String title, String subtitle, bool verified) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: height,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                image: DecorationImage(
+                  image: AssetImage(imageName),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 8,
+              right: 8,
+              child: Container(
+                padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.favorite_border,
+                  size: 20,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
         ),
-    )
-  ],
-  );
-}
+        SizedBox(height: 8),
+        Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 2),
+                  Row(
+                    children: [
+                      Text(
+                        subtitle,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.black
+                        ),
+                      ),
+                      if (verified) ...[
+                        SizedBox(width: 4),
+                        Image.asset(AppImages.verified,height: 10,width: 10,)
+                      ],
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         width: 345,
         decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(14),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(14),
         ),
-      
-          child: 
-                  GridView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  padding: EdgeInsets.all(18),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.7,
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 8,
-                    ),
-                    itemCount: images.length, 
-                    itemBuilder: (context,index){
-                      return postGrid(images[index]);
-                    }),
-          
-            
-        
+        child: MasonryGridView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          padding: EdgeInsets.all(18),
+          gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+          itemCount: images.length,
+          itemBuilder: (context, index) {
+            final item = images[index];
+            return postGrid(
+              item['image'],
+              item['height'],
+              item['title'],
+              item['subtitle'],
+              item['verified'],
+            );
+          },
+        ),
       ),
     );
   }
